ffbeca7b1503bce93434bd41b25eab59fbe58973
Khaledelsayed10 <llolococo123@gmail.com>
2024-11-30 22:42:39 +0200

    done app bar

---
+++ lib/pages/home_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/widget/custom_app_bar.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomAppBar();
+  }
+}
---
+++ lib/widget/custom_app_bar.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'News',
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            ),
+            Text(
+              'Cloud',
+              style: TextStyle(
+                color: Colors.yellow,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
