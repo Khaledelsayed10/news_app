a0bd9082781c8b20ab2b7ebbea24625623e20c88
Khaledelsayed10 <llolococo123@gmail.com>
2024-12-10 21:13:30 +0200

    add cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/cubit/cubit/list_view_cubit.dart';
 import 'package:news_app/pages/home_view.dart';
+import 'package:news_app/services/news_servic.dart';
 
 void main() {
   runApp(const NewsApp());
@@ -10,9 +14,12 @@ class NewsApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeView(),
+    return BlocProvider(
+      create: (context) => ListVeiwCubit(NewsServic(Dio())),
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomeView(),
+      ),
     );
   }
 }
--- lib/widget/news_list_view_builder.dart
+++ lib/widget/news_list_view_builder.dart
@@ -1,46 +1,36 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:news_app/model/article_model.dart';
-import 'package:news_app/services/news_servic.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/cubit/cubit/list_view_cubit.dart';
 import 'package:news_app/widget/newes_list_viwes.dart';
 
-class NewsListViewBuilder extends StatefulWidget {
-  const NewsListViewBuilder({super.key, required this.category});
+class NewsListViewBuilder extends StatelessWidget {
   final String category;
 
-  @override
-  State<NewsListViewBuilder> createState() => _NewsListViewBuilderState();
-}
-
-class _NewsListViewBuilderState extends State<NewsListViewBuilder> {
-  var future;
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    future = NewsServic(Dio()).getNews(
-      category: widget.category,
-    );
-  }
+  const NewsListViewBuilder({super.key, required this.category});
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<ArticleModel>>(
-      future: future,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return NewesListViwes(
-            articles: snapshot.data!,
-          );
-        } else if (snapshot.hasError) {
-          return const SliverToBoxAdapter(
-            child: Text('oops  was an error, try later'),
-          );
-        } else {
+    context.read<ListVeiwCubit>().getNews(category: category);
+
+    return BlocBuilder<ListVeiwCubit, ListVeiwState>(
+      builder: (context, state) {
+        if (state is ListVeiwLoding) {
           return const SliverToBoxAdapter(
             child: Center(
               child: CircularProgressIndicator(),
             ),
+          );
+        } else if (state is ListVeiwSaccess) {
+          return NewesListViwes(
+            articles: context.read<ListVeiwCubit>().articaList,
+          );
+        } else if (state is ListVeiwFailure) {
+          return const SliverToBoxAdapter(
+            child: Text('Oops! An error occurred, please try again later.'),
+          );
+        } else {
+          return const SliverToBoxAdapter(
+            child: Text('Unexpected state'),
           );
         }
       },
